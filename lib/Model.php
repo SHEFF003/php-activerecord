49901f53b85819a0115c5bac4fc1983c13180638
SHEFF <17074912+SHEFF003@users.noreply.github.com>
2019-11-04 22:09:57 +0300

    Update Model.php
    Fix count php 7.2

--- lib/Model.php
+++ lib/Model.php
@@ -1666,12 +1666,15 @@ class Model
 			$list = $table->find($options);
 		}
 		$results = count($list);
-
+		
+		if (!is_array($values)) {
+			$values = array($values);
+		}
+		
 		if ($results != ($expected = count($values)))
 		{
 			$class = get_called_class();
-			if (is_array($values))
-				$values = join(',',$values);
+			$values = join(',',$values);
 
 			if ($expected == 1)
 			{
